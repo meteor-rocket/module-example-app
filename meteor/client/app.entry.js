6d998f6964f046553b3956e367df6ddcd101fd33
Joseph Orbegoso Pea <joe@trusktr.io>
2015-08-01 16:27:05 -0700

    Update scene.

--- meteor/client/app.entry.js
+++ meteor/client/app.entry.js
@@ -1,11 +1,12 @@
 
 // famous stuff
-//import FamousEngine from 'famous/core/FamousEngine'
-//import Node         from 'famous/core/Node'
-//import Camera       from 'famous/components/Camera'
+import FamousEngine   from 'famous/core/FamousEngine'
+import DOMElement     from 'famous/dom-renderables/DOMElement'
+import Transitionable from 'famous/transitions/Transitionable'
+import Camera         from 'famous/components/Camera'
 
-import jss from 'jss'
-import jssNested from 'jss-nested'
+import jss            from 'jss'
+import jssNested      from 'jss-nested'
 
 //console.log(ExampleExport)
 
@@ -27,23 +28,42 @@ Template.body.helpers({
     }
 })
 
-Meteor.startup(function() {
-    //let scene = FamousEngine.createScene('body')
+Template.body.onRendered(function() {
+    console.log('Rendered the body.')
+    let scene = FamousEngine.createScene('body')
 
-    //let camera = new Camera(scene)
-    //camera.setDepth(1000)
+    let camera = new Camera(scene)
+    camera.setDepth(1000)
 
-    //let square = new Plane({
-        //size: [200,200],
-        //content: 'Hello.',
-        //properties: {
-            //backfaceVisibility: 'visible',
-            //background: 'pink',
-            //padding: '5px'
-        //}
-    //})
+    let squareNode = scene.addChild()
+    squareNode.setSizeMode(
+        'absolute', 'absolute', 'absolute'
+    )
+    squareNode.setAbsoluteSize(200,200,0)
+    squareNode.setAlign(0.5,0.5,0)
+    squareNode.setMountPoint(0.5,0.5,0)
 
-    //ctx.add(square)
-    //square.transform.setRotate([0,2*Math.PI,0], {duration: 5000, curve: 'easeInOut'})
+    let square = new DOMElement(squareNode, {
+        content: 'Hello.',
+        properties: {
+            backfaceVisibility: 'visible',
+            background: 'pink',
+            padding: '5px'
+        }
+    })
+
+    let rotation = new Transitionable(0)
+    rotation.set(2*Math.PI, {duration: 5000, curve: 'expoInOut'})
+
+    let rotationComponent = squareNode.addComponent({
+        onUpdate: function() {
+            console.log('asdfasdf')
+            squareNode.setRotation(0, rotation.get(), 0)
+            //if (!rotation.isActive())
+                //squareNode.requestUpdateOnNextTick(rotationComponent)
+        }
+    })
+    squareNode.requestUpdateOnNextTick(rotationComponent)
+
+    FamousEngine.init()
 })
-
