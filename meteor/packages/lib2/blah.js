027bd91cfa9ca3edf003cac2f9c6fe92a10638c4
Joseph Orbegoso Pea <joe@trusktr.io>
2015-08-19 16:59:37 -0700

    Removed local packages.

--- meteor/packages/lib2/blah.js
+++
@@ -1,1 +0,0 @@
-console.log('Hello on the server! This file is compiled by Meteor instead of rocket:module.')
--- meteor/packages/lib2/foo.js
+++
@@ -1,1 +0,0 @@
-console.log('Super duper heck yeah on the client!!!!! This file is loaded by Meteor instead of rocket:module.')
